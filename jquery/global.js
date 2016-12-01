a6a0127253bd76d03a7b929d7d36ad5ffd52711f
jinzhonghua <jinbangzhu@gmail.com>
2016-12-01 11:27:16 +0800

    init

---
+++ jquery/global.js
@@ -0,0 +1,5 @@
+const BASE_URL = "http://cty.zuvio.com.cn/app_v2/";
+const LOGIN_URL = BASE_URL + "login";
+const MAKE_ROLLCALL = BASE_URL + "makeRollcall";
+const GET_COURSE_LIST = BASE_URL + "getCourseList";
+const GET_ROLL_CALL = BASE_URL + "getRollcall";
